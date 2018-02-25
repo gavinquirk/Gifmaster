b2438bef6937ba157e81f88092757a39fe78e865
Gavin Quirk <gavinquirk@gmail.com>
2018-02-24 17:52:38 -0800

    added dynamic button creation

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,30 +1,31 @@
 
 
-var query = "placeholder"
+var query
 var apiKey = "MNhVzIgPRGBpvrTp4NUiu6Qy6AziD64G"
 var limit = 5
 var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + apiKey + "&limit=" + limit + ""
 var queryArr = ["cat", "dog", "panda"]
 
 
+
 createBtns()
+
+
 
 // Submit Button Click
 $("#subBtn").on("click", function () {
+    var searchTerm = $("#search").val()
     console.log("Submit Pressed")
-    console.log(queryArr)
-    queryArr.splice(0, 0, query)
+    queryArr.splice(0, 0, searchTerm)
     console.log(queryArr)
     eraseBtns()
     createBtns()
-
 })
-
 
 // Create buttons based on query array
 function createBtns() {
     for (i = 0; i < queryArr.length; i++) {
-        var newBtn = $("<button id='button-" + i + "' class='m-2' data-query=" + query + ">" + queryArr[i] + "</button>")
+        var newBtn = $("<button id='button-" + i + "' class='queryBtn m-2' data-query=" + queryArr[i] + ">" + queryArr[i] + "</button>")
         $("#btnCon").append(newBtn)
     }
 }
@@ -33,6 +34,12 @@ function createBtns() {
 function eraseBtns() {
     $("#btnCon").empty()
 }
+
+    // On query button click
+    $(".queryBtn").on("click", function() {
+        query = $(this).attr("data-query")
+        console.log(query)
+    })
 
 
 
@@ -44,8 +51,13 @@ $.ajax({
 
     console.log(response)
 
-    var img = $("<img src='"+response.data[0].images.fixed_height.url+"' class='border m-1'>")
-    $("#gifContainer").append(img)
+
+
+    // Temporary AJAX img request
+    // var img = $("<img src='" + response.data[i].images.fixed_height.url + "' class='border m-1'>")
+    // $("#gifContainer").append(img)
+
+
 
 
 })
