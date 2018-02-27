27bb725e5165b62002b7d2b371325a4839a01603
Gavin Quirk <gavinquirk@gmail.com>
2018-02-27 15:12:20 -0800

    changes to javascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,7 @@
 
 
-var query
-var apiKey = "MNhVzIgPRGBpvrTp4NUiu6Qy6AziD64G"
-var limit = 5
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + apiKey + "&limit=" + limit + ""
-var queryArr = ["cat", "dog", "panda"]
 
+var queryArr = ["cat", "dog", "panda"]
 
 
 createBtns()
@@ -17,12 +13,11 @@ $("#subBtn").on("click", function () {
     var searchTerm = $("#search").val()
     console.log("Submit Pressed")
     queryArr.splice(0, 0, searchTerm)
-    console.log(queryArr)
     eraseBtns()
     createBtns()
 })
 
-// Create buttons based on query array
+// Function: create buttons based on query array
 function createBtns() {
     for (i = 0; i < queryArr.length; i++) {
         var newBtn = $("<button id='button-" + i + "' class='queryBtn m-2' data-query=" + queryArr[i] + ">" + queryArr[i] + "</button>")
@@ -30,33 +25,42 @@ function createBtns() {
     }
 }
 
-// Erase buttons in container
+// Function: erase button container
 function eraseBtns() {
     $("#btnCon").empty()
 }
 
-    // On query button click
-    $(".queryBtn").on("click", function() {
-        query = $(this).attr("data-query")
-        console.log(query)
+// Function: erase gif container
+function eraseGifs() {
+    $("#gifContainer").empty()
+}
+
+
+
+// On query button click
+$(".queryBtn").on("click", function (event) {
+    event.preventDefault()
+    console.log(queryArr)
+    eraseGifs()
+    var apiKey = "MNhVzIgPRGBpvrTp4NUiu6Qy6AziD64G"
+    var limit = 15
+    query = $(this).attr("data-query")
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + apiKey + "&limit=" + limit + ""
+
+    // AJAX
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+
+        
+        // Create Gifs
+        for (r = 0; r < 10; r++) {
+            var img = $("<img src='" + response.data[r].images.fixed_height.url + "' class='border m-1'>")
+            $("#gifContainer").append(img)
+        }
     })
-
-
-
-// AJAX
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-
-    console.log(response)
-
-
-
-    // Temporary AJAX img request
-    // var img = $("<img src='" + response.data[i].images.fixed_height.url + "' class='border m-1'>")
-    // $("#gifContainer").append(img)
-
 
 
 
